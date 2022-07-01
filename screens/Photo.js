6134a40ab9243655d9e746d1a5388578e8218055
Fanch <fanch.daniel@gmail.com>
2022-07-01 16:20:55 +0200

    Réalisation de la page photo

--- screens/Photo.js
+++ screens/Photo.js
@@ -1,28 +1,50 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import React from "react";
-import { globalStyles } from "../style/AppStyles";
-
+import React from 'react'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import { globalStyles } from '../style/AppStyles';
 
 const Photo = ({ navigation }) => {
-  const handlePress = () => {
-    navigation.goBack();
-  };
+        const url = navigation.getParam('url');
+        const title = navigation.getParam('title');
+        const photoDesc = navigation.getParam('photoDesc');
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Photo</Text>
-      <Button title="Go Back" onPress={handlePress} />
-    </View>
-  );
-};
+    return (
+        <ScrollView style={globalStyles.container}>
+            <Image 
+                style={styles.selectedImage}
+                source={{uri: url}}
+            />
+            <View style={styles.photoDescription}>
+                <Text style={globalStyles.titleText}>{title}</Text>
+
+                <Text style={styles.textDescription}>{photoDesc}</Text>
+                <Text style={styles.textDescription}>{photoDesc}</Text>
+            </View>
+        </ScrollView>
+    )
+}
+
+Photo.navigationOptions = ({navigation}) => {
+    const photoTitle = navigation.getParam('title');
+    return {
+        headerTitle: photoTitle.toUpperCase()
+    }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#90ee90",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {},
-});
-export default Photo;
+    selectedImage: {
+        width: '100%',
+        height: 300
+    },
+    photoDescription: {
+        padding: 15,
+        fontSize: 25,
+        fontFamily: 'InriaSans_400Regular'
+    },
+    textDescription: {
+        fontFamily: 'InriaSans_400Regular',
+        fontSize: 20,
+        padding: 9
+    }
+})
+
+export default Photo
